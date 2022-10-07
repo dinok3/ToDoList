1f037bb6e46fe606ce3ef1fa3c3dd4d97331f9f7
Dino Krivic <dkrivic22@gmail.com>
2022-10-07 19:18:21 +0200

    .gitignore

--- users/views.py
+++ users/views.py
@@ -23,7 +23,7 @@ def profile(request):
     if request.method == "POST":
         user_form = UserUpdateForm(request.POST,request.FILES,instance=request.user)
         profile_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
-
+        
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
